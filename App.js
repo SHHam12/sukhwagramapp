9b4d66527a29e4842b9effd702d259a5ea9708bd
SHHam12 <hamsukhwa@gmail.com>
2018-01-17 21:12:28 -0500

    Creating AppContainer

--- App.js
+++ App.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import { AppLoading, Assets, Font } from "expo";
+import React from "react";
+import { AppLoading, Asset, Font } from "expo";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/es/integration/react";
-import configureStore from './redux/configureStore';
-
+import configureStore from "./redux/configureStore";
 const { persistor, store } = configureStore();
+import AppContainer from "./components/AppContainer";
 
-export default class App extends React.Component {
+class App extends React.Component {
   state = {
     isLoadingComplete: false
-  }
+  };
   render() {
     const { isLoadingComplete } = this.state;
     if (!isLoadingComplete) {
-      return (<AppLoading 
-          startAsync={this._loadAssetAsync}
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
           onError={this._handleLoadingError}
           onFinish={this._handleFinishLoading}
-          />
-        );
+        />
+      );
     }
     return (
-      <Provider store = {store}>
+      <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-          </View>
-          </PersistGate>
+          <AppContainer />
+        </PersistGate>
       </Provider>
     );
   }
-  _loadAssetAsync = async() => {
+  _loadAssetsAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
         require("./assets/images/logo.png"),
@@ -47,20 +46,13 @@ export default class App extends React.Component {
     ]);
   };
   _handleLoadingError = error => {
-    console.log(error);
+    console.error(error);
   };
-  _handleFinishLoading = async() => {
+  _handleFinishLoading = async () => {
     this.setState({
       isLoadingComplete: true
-    })
+    });
   };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
